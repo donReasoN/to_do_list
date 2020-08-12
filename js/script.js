7940b3b8f934ba0fb8380aa19ce551fa722cbab7
Sebastian Komorowski <sebastiano.komokomo@gmail.com>
2020-08-12 02:06:10 +0200

    Added render function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 {
+    const render = () => {
+        let stringElement = "";
+            
+        stringElement += `
+            <li class="tasks__item">
+                <button class="tasks__button tasks__button--done">
+                    <span class="tasks__icon fas fa-check"></span>
+                </button>
+
+                <span class="tasks__content">Example text</span>
+
+                <button class="tasks__button tasks__button--remove">
+                    <span class="tasks__icon fas fa-trash-alt"></span>
+                </button>
+            </li>
+        `;
+
+        document.querySelector('.js-tasks').innerHTML = stringElement;
+    }
+
     const onFormSubmit = (event) => {
         event.preventDefault();
     }
@@ -6,6 +26,7 @@
     const init = () => {
         const formElement = document.querySelector('.js-form');
         formElement.addEventListener('submit', onFormSubmit);
+        render();
     }
 
     init();
