978d7126649540b4b3e2864e4a306afdc6f3ae41
Sebastian Komorowski <sebastiano.komokomo@gmail.com>
2020-08-12 02:55:02 +0200

    Added toggleTask and removeTask function

--- js/script.js
+++ js/script.js
@@ -10,6 +10,26 @@
         },
     ];
 
+    const toogleTaskButton = () => {
+        const toogleButtons = document.querySelectorAll('.js-done');
+        toogleButtons.forEach((toogleButton, taskIndex) => {
+            toogleButton.addEventListener('click', () => {
+                tasksArray[taskIndex].done = !tasksArray[taskIndex].done;
+                render();
+            });
+        });
+    };
+
+    const removeTaskButton = () => {
+        const removeButtons = document.querySelectorAll('.js-remove');
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener('click', () => {
+                tasksArray.splice(taskIndex, 1);
+                render();
+            });
+        });
+    };
+    
     const addNewTask = () => {
         
         const newTask = document.querySelector('.js-newTask');
@@ -21,9 +41,9 @@
             });  
         } else {
             return null
-        }
+        };
         render();
-    }
+    };
 
     const render = () => {
         let stringElement = "";
@@ -31,26 +51,29 @@
         for (const task of tasksArray) {
             stringElement += `
             <li class="tasks__item">
-                <button class="tasks__button tasks__button--done">
+                <button class="tasks__button tasks__button--done js-done">
                     <span class="tasks__icon fas fa-check"></span>
                 </button>
 
-                <span class="tasks__content">${task.content}</span>
+                <span class="tasks__content ${task.done ? "task-content--done" : ""}">${task.content}</span>
 
-                <button class="tasks__button tasks__button--remove">
+                <button class="tasks__button tasks__button--remove js-remove">
                     <span class="tasks__icon fas fa-trash-alt"></span>
                 </button>
             </li>
         `;
-        }
+        };
         document.querySelector('.js-tasks').innerHTML = stringElement;
+
+        removeTaskButton();
+        toogleTaskButton();
     };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
 
         addNewTask();
-        
+
     };
 
     const init = () => {
