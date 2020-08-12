34e543b391187e4ad32a33c43d636cd18af11d46
Sebastian Komorowski <sebastiano.komokomo@gmail.com>
2020-08-12 02:14:14 +0200

    Added taskArray and 'for' loop

--- js/script.js
+++ js/script.js
@@ -1,33 +1,45 @@
 {
+    const tasksArray = [
+        {
+            content: 'Example 1',
+            done: false,
+        },
+        {
+            content: 'Example 2',
+            done: true,
+        },
+    ];
+
     const render = () => {
         let stringElement = "";
-            
-        stringElement += `
+        
+        for (const task of tasksArray) {
+            stringElement += `
             <li class="tasks__item">
                 <button class="tasks__button tasks__button--done">
                     <span class="tasks__icon fas fa-check"></span>
                 </button>
 
-                <span class="tasks__content">Example text</span>
+                <span class="tasks__content">${task.content}</span>
 
                 <button class="tasks__button tasks__button--remove">
                     <span class="tasks__icon fas fa-trash-alt"></span>
                 </button>
             </li>
         `;
-
+        }
         document.querySelector('.js-tasks').innerHTML = stringElement;
-    }
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-    }
+    };
 
     const init = () => {
         const formElement = document.querySelector('.js-form');
         formElement.addEventListener('submit', onFormSubmit);
         render();
-    }
+    };
 
     init();
-}
+};
